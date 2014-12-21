82733b376c4c67cc02657f78b4b8e320fed21bda
Michael Vorburger <mike@vorburger.ch>
2014-12-21 19:44:02 +0100

    v0.0.2: Toyed with ConfirmationActivity, failed, so back to basics

--- wear/src/main/java/calories/fit/vorburger/ch/foodcalories/MainWearActivity.java
+++ wear/src/main/java/calories/fit/vorburger/ch/foodcalories/MainWearActivity.java
@@ -1,9 +1,11 @@
 package calories.fit.vorburger.ch.foodcalories;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.support.wearable.activity.ConfirmationActivity;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.widget.TextView;
@@ -20,7 +22,10 @@ public class MainWearActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main_wear);
+        mTextView = (TextView) findViewById(R.id.text);
+/*
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
@@ -29,7 +34,7 @@ public class MainWearActivity extends Activity {
                 mTextView.setText("Ho ho ho.. say something! ;)");
             }
         });
-
+*/
         // https://developer.android.com/training/wearables/apps/voice.html
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
@@ -42,11 +47,25 @@ public class MainWearActivity extends Activity {
             // https://developer.android.com/training/wearables/apps/voice.html
             List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
             String spokenText = results.get(0);
+            Log.i(LOG_TAG, "HEARD: " + spokenText);
 
-            // Do something with spokenText
-            Log.i(LOG_TAG, "Heard " + spokenText);
-            mTextView.setText("You dixit: " + spokenText);
+            // TODO look up spoken item in our food database, and turn e.g. "apple" into "an apple" or "a bread" (with prefix)
+
+            String confirmationText = getString(R.string.msg_you_ate) + " an " + spokenText + " (+123 calories; now 456/789)";
+            // showSuccessConfirmation(this, confirmationText);
+            mTextView.setText(confirmationText);
         }
+        // NO } else {
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+/*
+    // This Confirmation always disappears immediately :-(
+    protected void showSuccessConfirmation(Context context, String message) {
+        Intent intent = new Intent(context, ConfirmationActivity.class);
+        intent.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE, ConfirmationActivity.SUCCESS_ANIMATION);
+        intent.putExtra(ConfirmationActivity.EXTRA_MESSAGE, message);
+        context.startActivity(intent);
+    }
+ */
 }
